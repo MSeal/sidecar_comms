3e8be13ab58fd4741c64ea332e59930b72b0acab
Dave Shoup <dave.shoup@gmail.com>
2022-12-23 16:11:34 -0500

    remove widget, update modeling

--- src/sidecar_comms/models.py
+++ src/sidecar_comms/models.py
@@ -1,23 +1,25 @@
-from typing import Any, Literal
+import enum
+from typing import Any, Literal, Optional
 
-from ipywidgets import Widget, register
 from pydantic import BaseModel, Field
-from traitlets import Unicode
+
+
+class SidecarRequestType(enum.Enum):
+    cell_ids = "cell_ids"
+    cell_states = "cell_states"
+    kernel_state = "kernel_state"
+
+
+class CommMessageBody(BaseModel):
+    request: Optional[SidecarRequestType]
+    data: Optional[dict[str, Any]]
+
+    class Config:
+        use_enum_values = True
 
 
 class CommMessage(BaseModel):
     source: Literal["sidecar_comms"] = "sidecar_comms"
-    body: dict[str, Any] = Field(default_factory=dict)
-    comm_id: str
-    target_name: str
-
-
-@register
-class BaseWidget(Widget):
-    _view_name = Unicode("SidecarComms").tag(sync=True)
-    _view_module = Unicode("sidecar_comms").tag(sync=True)
-    _view_module_version = Unicode("0.1.0").tag(sync=True)
-
-    @property
-    def value(self):
-        return self.comm.value
+    body: CommMessageBody = Field(default_factory=CommMessageBody)
+    comm_id: Optional[str] = None
+    target_name: Optional[str] = None
