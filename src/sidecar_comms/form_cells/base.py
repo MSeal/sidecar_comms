563c53e0fae7cf708f0325a5cbbb429798383323
Dave Shoup <dave.shoup@gmail.com>
2023-01-10 20:11:32 -0500

    add models

---
+++ src/sidecar_comms/form_cells/base.py
@@ -0,0 +1,93 @@
+from typing import Callable, Union
+
+from pydantic import parse_obj_as
+from traitlets import HasTraits, Int, Unicode
+
+from sidecar_comms.form_cells import models
+
+FormCellModel = Union[
+    models.DatetimePickerModel,
+    models.DropdownModel,
+    models.IntSliderModel,
+    models.MultiDropdownModel,
+    models.TextInputModel,
+]
+
+
+class FormCell:
+    def __init__(self, type: str, **kwargs):
+        self._type = type
+        self._parent_model = parse_obj_as(FormCellModel, {"type": self._type, **kwargs})
+        self._parent_traitlet = self._setup_traitlet()
+
+    def __repr__(self):
+        cell_name = self._parent_model.__class__.__name__.replace("Model", "")
+        props = ", ".join(f"{k}={v}" for k, v in self._parent_model.dict().items() if k != "type")
+        return f"<{cell_name} {props}>"
+
+    def _setup_traitlet(self):
+        traitlet = HasTraits()
+
+        traits_to_add = {}
+        for name, d in self._parent_model.schema()["properties"].items():
+            if name == "type":
+                continue
+            if d["type"] == "string":
+                traits_to_add[name] = Unicode()
+            elif d["type"] == "integer":
+                traits_to_add[name] = Int()
+
+        traitlet.add_traits(**traits_to_add)
+        traitlet.observe(self._sync_frontend, type="change")
+        return traitlet
+
+    def observe(self, fn: Callable, **kwargs):
+        self._parent_traitlet.observe(fn, **kwargs)
+
+    @property
+    def value(self):
+        return self._parent_model.value
+
+    @value.setter
+    def value(self, value):
+        self._parent_model.value = value
+        self._parent_traitlet.value = value
+
+    def _sync_frontend(self, change: dict):
+        print(f"send sidecar via comm_msg: {change=}")
+
+
+# --- Specific models ---
+
+
+class DatetimePicker(FormCell):
+    def __init__(self, **kwargs):
+        super().__init__(type="datetime_picker", **kwargs)
+
+
+class Dropdown(FormCell):
+    def __init__(self, **kwargs):
+        super().__init__(type="dropdown", **kwargs)
+
+    @property
+    def options(self):
+        return self._parent_model.options
+
+    @options.setter
+    def options(self, value):
+        self._parent_model.options = value
+
+
+class IntSlider(FormCell):
+    def __init__(self, **kwargs):
+        super().__init__(type="int_slider", **kwargs)
+
+
+class MultiDropdown(Dropdown):
+    def __init__(self, **kwargs):
+        super().__init__(type="multi_dropdown", **kwargs)
+
+
+class TextInput(FormCell):
+    def __init__(self, **kwargs):
+        super().__init__(type="text_input", **kwargs)
---
+++ src/sidecar_comms/form_cells/models.py
@@ -0,0 +1,36 @@
+from typing import Any, List, Literal
+
+from pydantic import BaseModel
+
+
+class DatetimePickerModel(BaseModel):
+    type: Literal["datetime_picker"] = "datetime_picker"
+    value: str
+    # TODO: ISO validation?
+
+
+class DropdownModel(BaseModel):
+    type: Literal["dropdown"] = "dropdown"
+    value: str
+    options: List[Any]
+
+
+class IntSliderModel(BaseModel):
+    type: Literal["int_slider"] = "int_slider"
+    value: int = 0
+    min: int = 0
+    max: int = 10
+    step: int = 1
+
+
+class MultiDropdownModel(BaseModel):
+    type: Literal["multi_dropdown"] = "multi_dropdown"
+    value: List[str]
+    options: List[Any]
+
+
+class TextInputModel(BaseModel):
+    type: Literal["text_input"] = "text_input"
+    value: str
+    min_length: int = 0
+    max_length: int = 1000
