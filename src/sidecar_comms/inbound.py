581cc8ffbe47a7b1c0c32edd620363d46473a378
Dave Shoup <dave.shoup@gmail.com>
2023-01-12 13:39:13 -0500

    handle form cell value update

--- src/sidecar_comms/inbound.py
+++ src/sidecar_comms/inbound.py
@@ -4,6 +4,7 @@ Comm target registration and message handling for inbound messages.
 """
 
 
+from sidecar_comms.form_cells.base import FORM_CELL_CACHE
 from sidecar_comms.handlers.main import get_kernel_variables
 
 
@@ -20,5 +21,14 @@ def inbound_comm(comm, open_msg):
         if data.get("msg") == "get_kernel_variables":
             msg = get_kernel_variables()
             comm.send(msg)
+
+        if data.get("msg") == "update_form_cell_value":
+            comm.send({"status": "updating form cell value"})
+            form_cell_id = data["form_cell_id"]
+            form_cell = FORM_CELL_CACHE[form_cell_id]
+            value = data["value"]
+            # TODO: handle when non-`value` attributes change
+            comm.send({"status": f"updating {form_cell_id} -> {str(form_cell)} with {value=}"})
+            form_cell.value = value
 
     comm.send({"status": "connected", "source": "sidecar_comms"})
