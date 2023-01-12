847e3f71513fb8593141d6fd9651086d1e6861a6
Kafonek <matt.kafonek@noteable.io>
2023-01-12 11:19:29 -0500

    comments and docstrings

--- src/sidecar_comms/form_cells/base.py
+++ src/sidecar_comms/form_cells/base.py
@@ -1,3 +1,31 @@
+"""
+Form Cells are our "better" version of widgets. On the frontend, they look like widgets but
+live in the cell input as opposed to being a cell output. When a cell is switched to "form",
+a cell metadata delta is pushed to the sidecar and sidecar sends the info on to the kernel
+over Comm.
+
+These models here represent different types of frontend form cells. A Datetime object in
+this file represents a date time picker form cell widget on the frontend. 
+
+The backend form cell objects have their state updated when frontend changes (user slides
+a slider or fills out a text input) through the same delta to sidecar -> sidecar comm to kernel
+flow as when a cell is first switched to "form".
+
+A user can programmatically create a form cell, such as typing model = Datetime() and outputting
+model in a cell. In that case, instead of ipython repr'ing the model, we get into the ipython
+display override and send a comm message to the sidecar to change the cell to "form".
+
+A user can also programmatically update the state of a form cell, and thanks to the ObservableModel
+pattern, those changes will be sent over Comm to the sidecar which will create cell metadata deltas
+that update the frontend state.
+
+Finally, the backend implementation of form cells use a discriminator pattern. You can create a
+form cell from a dictionary that is coming in from the frontend. Example:
+
+model = pydantic.parse_obj_as(FormCell, {"input_type": "datetime", "value": "2021-01-01"})
+model
+>>> Datetime(value=datetime.datetime(2021, 1, 1, 0, 0, tzinfo=datetime.timezone.utc))
+"""
 import uuid
 from datetime import datetime, timezone
 from typing import Annotated, Any, Dict, List, Literal, Union
@@ -11,6 +39,14 @@ FORM_CELL_CACHE: Dict[str, "FormCellBase"] = {}
 
 
 class FormCellBase(ObservableModel):
+    """
+    Base class for form cells.
+     - registers the class instance in the FORM_CELL_CACHE
+     - makes sure comm is open between sidecar and kernel
+     - when repr'd, override the ipython display handler and instead send a comm message so
+       that the sidecar will create a cell metadata delta that switches the cell type
+    """
+
     _comm: PrivateAttr
     id: str = Field(default_factory=lambda: str(uuid.uuid4()))
 
@@ -66,6 +102,8 @@ class Text(FormCellBase):
     max_length: int = 1000
 
 
+# FormCell is just a type, you can't instantiate FormCell()
+# See top of file / module docstring for example usage with pydantic.parse_obj_as
 FormCell = Annotated[
     Union[
         Datetime,
