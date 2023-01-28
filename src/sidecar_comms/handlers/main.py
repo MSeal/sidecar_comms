2aac810cdb58165ba54a4117c6b63396d68fb3d6
Dave Shoup <dave.shoup@gmail.com>
2023-01-27 19:17:03 -0500

    add inbound comm handler for variable renaming

--- src/sidecar_comms/handlers/main.py
+++ src/sidecar_comms/handlers/main.py
@@ -1,4 +1,5 @@
 from IPython import get_ipython
+from IPython.core.interactiveshell import InteractiveShell
 
 
 def get_kernel_variables():
@@ -11,3 +12,19 @@ def get_kernel_variables():
         "source": "sidecar_comms",
         "handler": "get_kernel_variables",
     }
+
+
+def rename_kernel_variable(
+    old_name: str,
+    new_name: str,
+    ipython_shell: InteractiveShell,
+) -> str:
+    """Renames a variable in the kernel."""
+    ipython = ipython_shell or get_ipython()
+    try:
+        if new_name:
+            ipython.user_ns[new_name] = ipython.user_ns[old_name]
+        del ipython.user_ns[old_name]
+        return "success"
+    except Exception as e:
+        return str(e)
