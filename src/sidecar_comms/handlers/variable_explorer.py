aa0e407a4ab3145553d9573d4c29482f3033a888
Dave Shoup <dave.shoup@gmail.com>
2023-02-10 16:22:32 -0500

    variable explorer updates (#32)
    * start VariableModel class
    
    * update tests
    
    * use helper functions instead of class
    
    * update test
    
    * value to sample_value
    
    * add comment

--- src/sidecar_comms/handlers/variable_explorer.py
+++ src/sidecar_comms/handlers/variable_explorer.py
@@ -4,38 +4,67 @@ from typing import Any, Optional
 
 from IPython import get_ipython
 from IPython.core.interactiveshell import InteractiveShell
+from pydantic import BaseModel
 
 
-def short_value(value: Any) -> str:
-    """Returns a short representation of a value."""
-    if isinstance(value, list):
-        return value[:5]
-    if isinstance(value, dict):
-        return value.keys()
-    if sys.getsizeof(value) > 1000:
-        return f"{value!r}"[:100] + "..."
-
-    try:
-        json.dumps(value)
-        return value
-    except Exception:
-        # non-JSON serializable
-        return f"{value!r}"[:100]
+class VariableModel(BaseModel):
+    name: str
+    sample_value: Any  # may be the full value if small enough, only truncated for larger values
+    type: str
+    size: Optional[int]
+    size_bytes: Optional[int]
 
 
-def get_memory_footprint(value: Any) -> Optional[int]:
-    """Returns the memory footprint of a value."""
+def variable_type(value: Any) -> str:
+    return type(value).__name__
+
+
+def variable_size(value: Any) -> Optional[int]:
+    if hasattr(value, "__len__"):
+        return len(value)
+    if hasattr(value, "size"):
+        return value.size
+
+
+def variable_size_bytes(value: Any) -> Optional[int]:
     try:
         return sys.getsizeof(value)
     except Exception:
         pass
-
     # may be a pandas object
     # TODO: add extra pandas object handlers
     try:
         return value.memory_usage().sum()
     except Exception:
         pass
+
+
+def variable_sample_value(value: Any) -> Any:
+    """Returns a short representation of a value."""
+    if isinstance(value, list):
+        # TODO: come back to this and maybe recursively check
+        # items in container
+        return value[:5]
+    if isinstance(value, dict):
+        return value.keys()
+    if variable_size_bytes(value) > 1000:
+        return f"{value!r}"[:1000] + "..."
+    try:
+        json.dumps(value)
+        return value
+    except Exception:
+        # non-JSON serializable
+        return f"{value!r}"[:1000]
+
+
+def variable_to_model(name: str, value: Any) -> VariableModel:
+    return VariableModel(
+        name=name,
+        sample_value=variable_sample_value(value),
+        type=variable_type(value),
+        size=variable_size(value),
+        size_bytes=variable_size_bytes(value),
+    )
 
 
 def get_kernel_variables(
@@ -58,13 +87,8 @@ def get_kernel_variables(
     for name, value in variables.items():
         if name.startswith(tuple(skip_prefixes)):
             continue
-        variable_types[name] = {
-            "name": name,
-            "type": type(value).__name__,
-            "size": len(value) if hasattr(value, "__len__") else 1,
-            "size_bytes": get_memory_footprint(value),
-            "value": short_value(value),
-        }
+        variable_model = variable_to_model(name=name, value=value)
+        variable_types[name] = variable_model.dict()
     return variable_types
 
 
