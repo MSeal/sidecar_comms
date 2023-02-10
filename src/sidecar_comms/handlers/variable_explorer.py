f73fb44380e4f1606c937e53d067e9c713d7f938
Dave Shoup <dave.shoup@gmail.com>
2023-02-10 09:37:14 -0500

    rename and change get_kernel_variables structure

--- src/sidecar_comms/handlers/variable_explorer.py
+++ src/sidecar_comms/handlers/variable_explorer.py
@@ -1,14 +1,47 @@
+import sys
 from typing import Any, Optional
 
 from IPython import get_ipython
 from IPython.core.interactiveshell import InteractiveShell
 
 
-def get_kernel_variables():
+def short_value(value: Any) -> str:
+    """Returns a short representation of a value."""
+    if isinstance(value, list):
+        return value[:5]
+    if isinstance(value, dict):
+        return value.keys()
+    if sys.getsizeof(value) > 1000:
+        return f"{value!r}"[:100] + "..."
+    return value
+
+
+def get_kernel_variables(
+    skip_prefixes: list = None, ipython_shell: Optional[InteractiveShell] = None
+):
     """Returns a list of variables in the kernel."""
-    ipython = get_ipython()
+    ipython = ipython_shell or get_ipython()
     variables = dict(ipython.user_ns)
-    variable_types = {name: str(type(value)) for name, value in variables.items()}
+
+    skip_prefixes = skip_prefixes or [
+        "_",
+        "In",
+        "Out",
+        "get_ipython",
+        "exit",
+        "quit",
+        "open",
+    ]
+    variable_types = {}
+    for name, value in variables.items():
+        if name.startswith(tuple(skip_prefixes)):
+            continue
+        variable_types[name] = {
+            "name": name,
+            "type": type(value).__name__,
+            "size": len(value) if hasattr(value, "__len__") else 1,
+            "value": short_value(value),
+        }
     return variable_types
 
 
