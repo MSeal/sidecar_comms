b2fc4d3467fcd310a87ad8aa4c6e567ef53855c4
Dave Shoup <dave.shoup@gmail.com>
2023-02-14 14:27:38 -0500

    remove variable explorer message updates

--- src/sidecar_comms/handlers/variable_explorer.py
+++ src/sidecar_comms/handlers/variable_explorer.py
@@ -1,6 +1,6 @@
 import json
 import sys
-from typing import Any, Optional, Tuple
+from typing import Any, Optional
 
 from pydantic import BaseModel
 
@@ -9,22 +9,10 @@ from sidecar_comms.shell import get_ipython_shell
 
 class VariableModel(BaseModel):
     name: str
-    type: str
-    docstring: Optional[str]
-    module: Optional[str]
     sample_value: Any  # may be the full value if small enough, only truncated for larger values
-    shape: Optional[Tuple]
+    type: str
     size: Optional[int]
     size_bytes: Optional[int]
-    error: Optional[str]
-
-
-def variable_docstring(value: Any) -> Optional[str]:
-    if (doc := getattr(value, "__doc__", None)) is None:
-        return
-    if not isinstance(doc, str):
-        return
-    return doc[:5000]
 
 
 def variable_type(value: Any) -> str:
@@ -32,32 +20,13 @@ def variable_type(value: Any) -> str:
 
 
 def variable_size(value: Any) -> Optional[int]:
-    """Returns the size (1-dimensional; length) of a variable."""
     if hasattr(value, "__len__"):
         return len(value)
-    if (size := getattr(value, "size", None)) is None:
-        return
-    if isinstance(size, int):
-        return size
-    if isinstance(size, tuple):
-        return size[0]
-
-
-def variable_shape(value: Any) -> Optional[int]:
-    """Returns the shape (n-dimensional; rows, columns, ...) of a variable."""
-    if (shape := getattr(value, "shape", None)) is None:
-        return
-    if not isinstance(shape, tuple):
-        return
-    if not shape:
-        return
-    if not isinstance(shape[0], int):
-        return
-    return shape
+    if hasattr(value, "size"):
+        return value.size
 
 
 def variable_size_bytes(value: Any) -> Optional[int]:
-    """Returns the size of a variable in bytes."""
     try:
         return sys.getsizeof(value)
     except Exception:
@@ -89,33 +58,13 @@ def variable_sample_value(value: Any) -> Any:
 
 
 def variable_to_model(name: str, value: Any) -> VariableModel:
-    """Gathers properties of a variable to send to the sidecar through
-    a variable explorer comm message.
-    Should always have `name` and `type` properties; `error` will show
-    conversion/inspection errors for size/size_bytes/sample_value.
-    """
-    basic_props = {
-        "name": name,
-        "docstring": variable_docstring(value),
-        "type": variable_type(value),
-        "module": getattr(value, "__module__", None),
-    }
-
-    # in the event we run into any parsing/validation errors,
-    # we'll still send the variable model with basic properties
-    # and an error message
-    try:
-        return VariableModel(
-            sample_value=variable_sample_value(value),
-            shape=variable_shape(value),
-            size=variable_size(value),
-            size_bytes=variable_size_bytes(value),
-            **basic_props,
-        )
-    except Exception as e:
-        basic_props["error"] = str(e)
-
-    return VariableModel(**basic_props)
+    return VariableModel(
+        name=name,
+        sample_value=variable_sample_value(value),
+        type=variable_type(value),
+        size=variable_size(value),
+        size_bytes=variable_size_bytes(value),
+    )
 
 
 def get_kernel_variables(skip_prefixes: list = None):
