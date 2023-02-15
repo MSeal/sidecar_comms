2c87ac305ce15926517017a6d1532b916f7ea4cd
Dave Shoup <dave.shoup@gmail.com>
2023-02-15 11:20:23 -0500

    shell cleanup (#34)
    * add singleton shell and remove optional ipython_shell kwargs for testing
    
    * wip
    
    * more VE updates
    
    * add docstring, fix tuple
    
    * remove variable explorer message updates
    
    * these were bothering me

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,8 @@
 import pytest
 from ipykernel.comm import Comm
 from IPython.core.interactiveshell import InteractiveShell
-from IPython.testing import tools
+
+from sidecar_comms.shell import Shell
 
 
 @pytest.fixture
@@ -9,18 +10,9 @@ def sample_comm() -> Comm:
     return Comm(target_name="test")
 
 
-@pytest.fixture
-def get_ipython() -> InteractiveShell:
-    if InteractiveShell._instance:
-        shell = InteractiveShell.instance()
-    else:
-        config = tools.default_config()
-        config.TerminalInteractiveShell.simple_prompt = True
-        shell = InteractiveShell.instance(config=config)
-
-    # clear out any lingering variables between tests
-    orig_variables = dict(shell.user_ns).copy()
-
-    yield shell
-
-    shell.user_ns = orig_variables
+@pytest.fixture(autouse=True)
+def tmp_ipython() -> InteractiveShell:
+    test_shell = InteractiveShell.instance()
+    Shell()._instance = test_shell
+    yield test_shell
+    Shell()._instance = None
